491c9f19abec1ce24004db12347bcd5f74824490
minmin1805 <doanlyminh2005@gmail.com>
2025-06-27 01:48:33 -0400

    small fix to font size in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
     
         <div className='flex flex-col items-center p-10 py-20 gap-6 h-[650px] w-full bg-blue-50'>
         <h2 className='text-lg'>Welcome to CarCentral - Your Ultimate Car Marketplace</h2>
-        <h2 className='text-[60px] font-bold'>Find your dream car</h2>
+        <h2 className='text-[50px] font-bold text-center'>Search to find the best car for you</h2>
         <Search />
         <img src='/file.png' className='mt-10 w-[900px] h-auto'/>
         </div>
