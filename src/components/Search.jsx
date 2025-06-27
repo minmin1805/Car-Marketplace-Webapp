c1a4ed2848179aa1c3b4c592eadb2a81f377edad
minmin1805 <doanlyminh2005@gmail.com>
2025-06-27 01:08:32 -0400

    improve the design for search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,70 +1,67 @@
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { IoIosSearch } from "react-icons/io";
-import React from 'react'
+import React from "react";
 import Data from "@/Shared/Data";
 import { Link } from "react-router-dom";
 import { Separator } from "./ui/separator";
 
 function Search() {
-
-  const [cars, setCars]=React.useState([]);
-  const [make, setMake]=React.useState([]);
-  const [price, setPrice]=React.useState([]);
-
+  const [cars, setCars] = React.useState([]);
+  const [make, setMake] = React.useState([]);
+  const [price, setPrice] = React.useState([]);
 
   return (
-    <div className='p-2 md:p-5 bg-white rounded-md md:rounded-full flex-col md:flex md:flex-row gap-10 px-5 items-center w-[60%]'>
-    <Select onValueChange={(value)=> setCars(value)}>
-  <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
-    <SelectValue placeholder="Cars" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="New">New</SelectItem>
-    <SelectItem value="Used">Used</SelectItem>
-    <SelectItem value="Certified Pre-Owned">Certified Pre-Owned</SelectItem>
-  </SelectContent>
-</Select>
+    <div className="flex flex-row gap-10 p-5 bg-white rounded-md md:rounded-full justify-center items-center">
+      <Select onValueChange={(value) => setCars(value)}>
+        <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Cars" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="New">New</SelectItem>
+          <SelectItem value="Used">Used</SelectItem>
+          <SelectItem value="Certified Pre-Owned">
+            Certified Pre-Owned
+          </SelectItem>
+        </SelectContent>
+      </Select>
 
-<Separator orientation="vertical" className="hidden md:block"/>
+      <Separator orientation="vertical" className="hidden md:block" />
 
-<Select onValueChange={(value) =>setMake(value)}>
-  <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
-    <SelectValue placeholder="Car Makers" />
-  </SelectTrigger>
-  <SelectContent>
-  {Data.CarMakers.map((maker, index) => (
-    <SelectItem value={maker.name}>{maker.name}</SelectItem>
-  ))
-  }
-  </SelectContent>
-</Select>
+      <Select onValueChange={(value) => setMake(value)}>
+        <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Car Makers" />
+        </SelectTrigger>
+        <SelectContent>
+          {Data.CarMakers.map((maker, index) => (
+            <SelectItem value={maker.name}>{maker.name}</SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
 
-<Separator orientation="vertical" className="hidden md:block"/>
+      <Separator orientation="vertical" className="hidden md:block" />
 
+      <Select onValueChange={(value) => setPrice(value)}>
+        <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Pricing" />
+        </SelectTrigger>
+        <SelectContent>
+          {Data.Pricing.map((price, index) => (
+            <SelectItem value={price.amount}>{price.amount}</SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
 
-<Select onValueChange={(value) => setPrice(value)}>
-  <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
-    <SelectValue placeholder="Pricing" />
-  </SelectTrigger>
-  <SelectContent>
-  {Data.Pricing.map((price, index) => (
-    <SelectItem value={price.amount}>{price.amount}</SelectItem>
-  ))}
-  </SelectContent>
-</Select>
-
-<Link to={'/search?cars='+cars+"&make="+make+"&price="+price}>
-<IoIosSearch className='text-[50px] bg-primary rounded-full p-4 text-white hover:scale-105 transition-all cursor-pointer'/>
-</Link>
-
-</div>
-  )
+      <Link to={"/search?cars=" + cars + "&make=" + make + "&price=" + price}>
+        <IoIosSearch className="text-[50px] bg-primary rounded-full p-4 text-white hover:scale-105 transition-all cursor-pointer" />
+      </Link>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
